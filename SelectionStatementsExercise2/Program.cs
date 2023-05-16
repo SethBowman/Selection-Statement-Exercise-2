7f34641c998c85deaf40a2838f33673cace2b4c0
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-16 11:58:45 -0500

    Exercise completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("what is your favorite subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I dislike math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules");
+                    break;
+                case "pe":
+                case "p.e.":
+                    Console.WriteLine("P.E. is awesome");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite");
+                    break;
+                default:
+                    Console.WriteLine("I forgot about that one");
+                    break;
+            }
         }
     }
 }
